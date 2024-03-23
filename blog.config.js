f94a7070b92561a700a2f205f5039f0ca61d5a33
xiaobu009 <140291001+xiaobu009@users.noreply.github.com>
2024-03-23 09:16:03 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,9 +20,9 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  BIO: process.env.NEXT_PUBLIC_BIO || '每一步都是探索，每一步都有收获，每一步都在成长🍚', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.smallstep.one', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '网络创业 网络赚钱 视频制作 被动收入 在线收入 AI工具教程,AI创新,在线赚钱,被动收益策略,视频制作工具,网上创业,免费创业工具,人工智能创业,在线赚钱的实用技巧和方法,AI工具在创业中的应用案例,免费创业工具的推荐和教程,人工智能创新与创业机会', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
