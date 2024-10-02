56a52abe5378a1bc3be3e25ea33b82f1c8cb2eb4
xiaobu009 <ofirst1@gmail.com>
2024-10-02 09:18:31 +0800

    更新 Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -22,7 +22,7 @@ const Footer = ({ title }) => {
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1>
         <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>Powered by <a href='https://github.com/tangly1024/NotionNext' className='dark:text-gray-300'>NotionNext {siteConfig('VERSION')}</a>.</p></span><br/>
-        <p>隐私条款 </p>
+        <p className='text-xs pt-2 text-light-500 dark:text-gray-500'><a ref='#' className='dark:text-gray-300'>隐私条款</a</p>
     </footer>
   )
 }
